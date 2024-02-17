49e48488ebbbf2573c6e9a4057bb9e8041f74d25
Maurice Faber <morriz@idiotz.nl>
2024-02-17 16:02:08 +0100

    fix: do not git pull in dev repo...doh!

--- lib/git.py
+++ lib/git.py
@@ -1,12 +1,20 @@
+import os
+
+from dotenv import load_dotenv
+
 from lib.proxy import reload_proxy, write_nginx
 from lib.upstream import update_upstreams, write_upstreams
 from lib.utils import run_command
+
+load_dotenv()
 
 
 def update_repo() -> None:
     """Update the local git repo"""
     # execute a git pull with python in the root of this project:
-    run_command(["git", "pull"], cwd=".")
+    if os.environ["PYTHON_ENV"] == "production":
+        run_command("git fetch origin main".split(" "), cwd=".")
+        run_command("git reset --hard origin/main".split(" "), cwd=".")
     write_nginx()
     write_upstreams()
     update_upstreams()
