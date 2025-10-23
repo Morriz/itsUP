96f9567b2f9c569bc37cb89e9717094461822263
Maurice Faber <maurice@instrukt.ai>
2025-10-24 00:17:32 +0200

    simplify logs-api.sh - no JSON parsing needed

--- bin/logs-api.sh
+++ bin/logs-api.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 
-tail -f logs/*.log | while read line; do if
-  echo $line | egrep "\{.*}" >/dev/null
-  json=$(echo $line | egrep -oh "\{.*}" | jq '.')
-then echo $line | awk -v json="$json" -F "\{.*}" '{printf "%s%s%s\n",$1,json,$2}'; fi; done
+tail -f logs/*.log
